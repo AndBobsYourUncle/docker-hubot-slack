82e1117e3fc8db0f52bf22a114fe75684e6b69ba
Levi Smith <levi@fynx.me>
2016-03-15 15:59:38 -0500

    changing mv to copy in prod/dev scripts

--- devmode.sh
+++ devmode.sh
@@ -3,7 +3,7 @@
 echo "Initiating Dev Mode"
 
 rm -f /etc/supervisord.d/*
-mv ${HUBOT_HOME}/dev/supervisord.ini /etc/supervisor.d/supervisord.ini
+cp -f ${HUBOT_HOME}/dev/supervisord.ini /etc/supervisor.d/supervisord.ini
 
 cd ${HUBOT_HOME}
 source ./config/hubot.conf
--- prodmode.sh
+++ prodmode.sh
@@ -3,7 +3,7 @@
 echo "Initiating Prod Mode"
 
 rm -f /etc/supervisord.d/*
-mv ${HUBOT_HOME}/prod/supervisord.ini /etc/supervisor.d/supervisord.ini
+cp -f ${HUBOT_HOME}/prod/supervisord.ini /etc/supervisor.d/supervisord.ini
 
 cd ${HUBOT_HOME}
 source ./config/hubot.conf
